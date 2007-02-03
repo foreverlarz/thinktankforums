377d54d18302c23a610638b4d18a50cd68805c1f
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2007-02-03 01:27:00 +0000

    comment cleanup

--- common.inc.php
+++ common.inc.php
@@ -33,41 +33,44 @@
  */
 
 
-// message printing //
-function message($section, $header, $message, $a, $b) {
+// message printing
+
+ function message($section, $header, $message, $a, $b) {
 	global $ttf;
 	if ($a == 1) {
 		$label = $section;
 		include "header.inc.php";
 	};
- ?>
+?>
   <table border="0" cellpadding="2" cellspacing="1" width="600" class="shift">
    <tr class="mediuminv"><td width="594" colspan="5"><b><?php echo $header; ?></b></td></tr>
    <tr class="medium"><td><?php echo $message; ?></td></tr>
   </table>
 <?php
 	if ($b == 1) include "footer.inc.php";
-};
+ };
 
 
-// validate as admin //
-function admin() {
+// validate as admin
+
+ function admin() {
 	global $ttf;
 	if ($ttf["uid"] != 1 && $ttf["uid"] != 2) {
 	 message("ttf administration backend!","dead end.","please do not reload this page or attempt to exploit it. every <b>page view</b> is logged with {ip, timestamp, request, additional agent information}.",1,1);
 	 die();
 	};
-};
+ };
 
 
-// input security cleaning //
-function clean($input) {	// allows html, only escapes
+// input security cleaning
+
+ function clean($input) {	// allows html, only escapes
 	if (get_magic_quotes_gpc()) {
 		$input = stripslashes($input);
 	};
 	$output = mysql_real_escape_string(trim($input));
 	return($output);
-};
+ };
 
 
 /* convert text blobs for output //
@@ -124,36 +127,42 @@ function outputbody($input)
 };
 
 // mysql error printing
-function showerror() {
+
+ function showerror() {
 	if (mysql_error()) {
 		message("fatal error", "mysql dbms", "error ".mysql_errno().": ".mysql_error(), 1, 1); die();
 	} else {
 		message("fatal error", "mysql dbms", "could not connect.", 1, 1); die();
 	};
-};
+ };
 
 
-/* database variables
+/* database variables //
+ *
  * let $dbms_host be the hostname
  *     $dbms_user be the username
  *     $dbms_pass be the password
  *     $dbms_db   be the database
  */
-require "../credentials.inc.php";
+
+ require "../credentials.inc.php";
 
 
 // mysql dbms connection
+
  if (!($dbmscnx = @mysql_pconnect($dbms_host, $dbms_user, $dbms_pass))) showerror();
  if (!mysql_select_db($dbms_db)) showerror();
 
 
 // config variables
+
  $result = mysql_query("SELECT * FROM ttf_config");
  while ($config = mysql_fetch_array($result)) $ttf_config["{$config["name"]}"] = $config["value"];
  mysql_free_result($result);
 
 
 // check banned list
+
  $sql = "SELECT * FROM ttf_banned GROUP BY ip";
  $result = mysql_query($sql);
  while ($ban = mysql_fetch_array($result)) {
@@ -166,6 +175,7 @@ require "../credentials.inc.php";
 
 
 // cookie management
+
  if (isset($_COOKIE["thinktank"])) {
   list($user_id, $password) = unserialize(stripslashes($_COOKIE["thinktank"]));
   $result = mysql_query("SELECT user_id, username, avatar_type, time_zone FROM ttf_user WHERE user_id='$user_id' AND password='$password'");
@@ -185,6 +195,7 @@ require "../credentials.inc.php";
 
 
 // maintenance
+
  if ($ttf_config["maintenance"] && $ttf["uid"] != 1) {
   message("think tank forums!","maintenance","sorry, ttf is offline for maintenance.<br />we are most likely updating scripts and adding new features! come back soon!\n", 1, 1);
   die();
