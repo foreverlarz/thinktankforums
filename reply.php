5cec70bf60ba67dee27466e2acd815df1477003d
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2007-03-04 04:13:20 +0000

    r62 broke reply.php. but now it's almost completely rewritten! (ooh! pretty!)
    i'm a lot nicer about comments and readable code now, huh?

--- reply.php
+++ reply.php
@@ -2,35 +2,57 @@
 /* think tank forums
  *
  * reply.php
- *
- * this script accepts the following variables:
- * 	$_POST["thread_id"]	clean
- *	$_POST["body"]		clean
- *
- * sanity checks include:
- * 	valid user logged in
- * 	neither thread_id nor body are blank
- * 	valid forum and thread specified
- * 	includes are REQUIRED
- *
- * NOTE: THIS IS A SILENT SCRIPT.
  */
+
 require "common.inc.php";
+
 $thread_id = clean($_POST["thread_id"]);
 $body = clean($_POST["body"]);
+
 if (isset($ttf["uid"])) {
-   if ($thread_id != "" && $body != "") {
-      $resulta = mysql_query("SELECT forum_id FROM ttf_thread WHERE thread_id='$thread_id' LIMIT 1");
-      $thread = mysql_fetch_array($resulta);
-      mysql_free_result($resulta);
-      if (isset($thread["forum_id"])) {
-         $resultb = mysql_query("INSERT INTO ttf_post VALUES ('', '$thread_id', '{$ttf["uid"]}', UNIX_TIMESTAMP(), '{$_SERVER["REMOTE_ADDR"]}', '$body')");
-         $resultc = mysql_query("UPDATE ttf_thread SET date=UNIX_TIMESTAMP(), posts=posts+1 WHERE thread_id='$thread_id'");
-         $resultd = mysql_query("UPDATE ttf_forum SET date=UNIX_TIMESTAMP(), posts=posts+1 WHERE forum_id='{$thread["forum_id"]}'");
-         $resulte = mysql_query("REPLACE INTO ttf_thread_new SET thread_id='$thread_id', user_id='{$ttf["uid"]}', last_view=UNIX_TIMESTAMP()");
-         $resultf = mysql_query("UPDATE ttf_user SET post_date=UNIX_TIMESTAMP() WHERE user_id='{$ttf["uid"]}'");
-      };
-   };
+
+	if ($thread_id != '' && $body != '') {
+
+		// select the $forum_id of the forum that the post is being inserted into
+		$sql = "SELECT forum_id FROM ttf_thread WHERE thread_id='$thread_id' LIMIT 1";
+		$result = mysql_query($sql);
+		list($forum_id) = mysql_fetch_array($result);
+		mysql_free_result($result);
+		if (!$forum_id) {
+			message("fatal error", "fatal error", "the thread specified does not exist.", 1, 1);
+			die();
+		};
+
+		// insert the post into the respective thread
+		$sql = "INSERT INTO ttf_post SET thread_id='$thread_id', author_id='{$ttf["uid"]}', ". 
+			"date=UNIX_TIMESTAMP(), ip='{$_SERVER["REMOTE_ADDR"]}', body='$body'";
+         	if (!$result = mysql_query($sql)) showerror();
+
+		// update the thread's post count and date
+		$sql = "UPDATE ttf_thread SET date=UNIX_TIMESTAMP(), posts=posts+1 WHERE thread_id='$thread_id'";
+         	if (!$result = mysql_query($sql)) showerror();
+
+		// update the forum's post count and date
+		$sql = "UPDATE ttf_forum SET date=UNIX_TIMESTAMP(), posts=posts+1 WHERE forum_id='$forum_id'";
+         	if (!$result = mysql_query($sql)) showerror();
+
+		// mark the thread as read for the author
+		$sql = "REPLACE INTO ttf_thread_new SET thread_id='$thread_id', user_id='{$ttf["uid"]}', ".
+			"last_view=UNIX_TIMESTAMP()";
+         	if (!$result = mysql_query($sql)) showerror();
+
+		// upate the user's last post date
+		$sql = "UPDATE ttf_user SET post_date=UNIX_TIMESTAMP() WHERE user_id='{$ttf["uid"]}'";
+         	if (!$result = mysql_query($sql)) showerror();
+
+		header("Location: thread.php?thread_id=$thread_id");
+
+	} else {
+		message("fatal error", "fatal error", "either the thread_id or body fields were left empty.", 1, 1);
+	};
+
+} else {
+	message("fatal error", "fatal error", "you must be logged in to post.", 1, 1);
 };
-header("Location: thread.php?thread_id=$thread_id");
+
 ?>
