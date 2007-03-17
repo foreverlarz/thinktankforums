78b53c5302c6eb2ce752cfefafd3887bac274cbf
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2007-03-17 08:10:45 +0000

    comment the "page generated in ..." text
    blah!

--- include_footer.php
+++ include_footer.php
@@ -6,13 +6,11 @@
 ?>
             <!-- **** **** begin footer.inc.php **** **** -->
             <br style="clear: both;" />
-            <span class="small">
-<?php
-$time_end = microtime(true);
-$time = $time_end - $time_start;
-echo "                page generated in $time seconds\n";
-?>
-            </span>
         </div>
     </body>
 </html>
+<?php
+$time_end = microtime(true);
+$time = $time_end - $time_start;
+echo "<!-- page generated in $time seconds -->";
+?>
