1943729f17a2c048c74fd6113f7ccc535f112182
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2007-03-18 23:37:00 +0000

    force charset=utf-8 in the http header
    why doesn't the meta tag work by itself? if apache is serving with a default charset of iso-8859-1 and i only use the meta tag to change the charset to utf-8, both firefox and ie still use iso-8859-1. why doesn't the meta tag override the http header?

--- include_header.php
+++ include_header.php
@@ -3,12 +3,15 @@
  *
  * header.inc.php
  */
+
+header('Content-Type: text/html; charset=utf-8');
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
     <head>
-        <title>think tank forums <?php echo $ttf_config["version"]; ?></title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>think tank forums <?php echo $ttf_config["version"]; ?></title>
         <script type="text/javascript" src="script_confirm.js"></script>
         <link rel="stylesheet" type="text/css" href="style.css" />
         <!--[if lt IE 7]>
