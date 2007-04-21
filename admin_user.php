45241a8ee60bda4303a1f76c058ff9253a0b2b35
fakeempire <fakeempire@510118ab-9126-0410-8522-355556fb886b>
2007-04-21 21:41:01 +0000

    added two functions to include_common.php - banUser() and unbanUser().
    added ban column to admin_user.php.
    
    admin_ban.php is no longer needed (if this is accepted). also admin_userinfo.php will need to be updated with a new link for 'ban this user'

--- admin_user.php
+++ admin_user.php
@@ -13,6 +13,22 @@ $label = "administration &raquo; user list";
 
 require_once "include_header.php";
 
+
+if (isset($_GET["banuser"])) {
+        
+    $user_id = clean($_GET["banuser"]);
+
+    banUser($user_id);
+}
+
+else if (isset($_GET["unbanuser"])) {
+        
+    $user_id = clean($_GET["unbanuser"]);
+
+    unbanUser($user_id);
+
+} else { 
+
 ?>
             <table cellspacing="1" class="content">
                 <thead>
@@ -21,12 +37,13 @@ require_once "include_header.php";
                         <th>username</th>
                         <th>email</th>
                         <th>last visit</th>
+                        <th>ban user</th>
                     </tr>
                 </thead>
                 <tbody>
 <?php
 
-$sql = "SELECT user_id, username, email, visit_date ".
+$sql = "SELECT user_id, username, email, visit_date, perm ".
        "FROM ttf_user ORDER BY user_id";
 if (!$result = mysql_query($sql)) showerror();
 
@@ -49,6 +66,20 @@ while ($user = mysql_fetch_array($result)) {
                         <td><a href="admin_userinfo.php?user_id=<?php echo $user["user_id"]; ?>"><?php echo output($user["username"]); ?></a></td>
                         <td><a href="mailto:<?php echo $user["email"]; ?>"><?php echo $user["email"]; ?></a></td>
                         <td<?php echo $hl; ?>><?php echo formatdate($user["visit_date"]); ?></td>
+                        <td><a href="admin_user.php?<?php 
+                            if ($user["perm"] != "banned") {
+
+                                echo "banuser=".$user["user_id"]."\">ban</a>";
+
+                            } else {
+                                
+                                echo "unbanuser=".$user["user_id"]."\">unban</a>";
+
+                            }
+
+                            ?>
+                        </td>
+                            
                     </tr>
 <?php
 
@@ -60,6 +91,8 @@ mysql_free_result($result);
                 </tbody>
             </table>
 <?php
+
+};
 
 require_once "include_footer.php";
 
--- include_common.php
+++ include_common.php
@@ -523,4 +523,163 @@ if ($ttf_config["maintenance"] && $ttf["perm"] != 'admin') {
 
 };
 
+
+/* banuser
+ * ~~~~~~~~~~~~~~~~
+ * this code allows the administration to block access to the forums
+ * for a specific user, while also banning the register_ip and all 
+ * visit_ip's as well. 
+ */
+
+function banUser($user_id) {
+    admin();  // I dont think this is necessary
+
+    $sql = "SELECT perm, username, register_ip, visit_ip FROM ttf_user WHERE user_id='$user_id'";
+    if (!$result = mysql_query($sql)) showerror();
+    $user = mysql_fetch_array($result);
+    mysql_free_result($result);
+
+    if (!empty($user["perm"]) && $user["perm"] != "banned") {
+
+        if (!empty($user["register_ip"])) {
+            $sql = "REPLACE INTO ttf_banned ".
+                "SET user_id='$user_id', ip='{$user["register_ip"]}'";
+            if (!$result = mysql_query($sql)) {
+                showerror();
+            } else {
+                $arrMessage[] = "register ip of " .$user["register_ip"]." was banned";
+            };
+        } else {
+            $arrMessage[] = "<span class=\"error\">no register ip for this user</span>";
+        };
+
+
+        if (!empty($user["visit_ip"])) {
+            $sql = "REPLACE INTO ttf_banned ".
+              "SET user_id='$user_id', ip='{$user["visit_ip"]}'";
+            if (!$result = mysql_query($sql)) {
+                showerror();
+            } else {
+                $arrMessage[] = "visit ip of ".$user["visit_ip"]." was banned";
+            };
+        } else {
+            $arrMessage[] = "<span class=\"error\">no visit ip for this user</span>";
+        };
+
+
+        $sql = "SELECT post_id, ip FROM ttf_post WHERE author_id='$user_id' ".
+           "&& ip IS NOT NULL GROUP BY ip";
+        if (!$result = mysql_query($sql)) showerror();
+
+        while ($post = mysql_fetch_array($result)) {
+
+            $sql = "REPLACE INTO ttf_banned SET user_id='$user_id', ip='{$post["ip"]}'";
+            if (!$resultx = mysql_query($sql)) {
+                showerror();
+            } else {
+                $arrMessage[] = "post ip of ".$user["post_ip"]." was banned";
+            };
+	    };
+	    mysql_free_result($result);
+
+
+        $sql = "SELECT date, ip FROM ttf_visit ".
+           "WHERE user_id='$user_id' && ip<>'' GROUP BY ip";
+        if (!$result = mysql_query($sql)) showerror();
+
+        while ($visit = mysql_fetch_array($result)) {
+
+            $sql = "REPLACE INTO ttf_banned SET user_id='$user_id', ip='{$visit["ip"]}'";
+            if (!$resultx = mysql_query($sql)) {
+                showerror();
+            } else {
+                $arrMessage[] = "visit ip of ".$visit["ip"]." was banned";
+            };
+    	};
+	    mysql_free_result($result);
+
+
+        $sql = "UPDATE ttf_user SET perm='banned' WHERE user_id='$user_id'";
+        if (!$result = mysql_query($sql)) {
+            showerror();
+            } else {
+                $arrMessage[] = $user["username"] ." -> banned!";
+            };
+
+
+    } elseif ($user["perm"] == "banned") {
+
+        $arrMessage[] = "<span class=\"error\">the user_id provided is already banned</span>";
+
+    } else {
+
+        $arrMessage[] = "<span class=\"error\">the user_id provided is invalid</span>";
+
+    };
+
+    if (!empty($arrMessage)) {
+    
+        message("banuser", "information", $arrMessage);
+    
+    }; 
+};
+
+/* unbanuser
+ * ~~~~~~~~~~~~~~~~
+ * this code allows the administration to unblock access to the forums
+ * for a specific user, while also unbanning the register_ip and all 
+ * visit_ip's as well. 
+ */
+function unbanUser($user_id) {
+    admin(); // I dont think this one is necessary either - see banUser()
+
+    $sql = "SELECT perm, username, register_ip, visit_ip FROM ttf_user WHERE user_id='$user_id'";
+    if (!$result = mysql_query($sql)) showerror();
+    $user = mysql_fetch_array($result);
+    mysql_free_result($result);
+
+    if ($user["perm"] == "banned") {
+
+        $sql = "DELETE FROM ttf_banned WHERE user_id='$user_id' ";
+
+        if (!$result = mysql_query($sql)) {
+                
+            showerror();
+
+        } else {
+
+            $arrMessage[] = $user["username"] ."'s ip's where unbanned";
+
+        };
+
+
+        $sql = "UPDATE ttf_user SET perm='user' WHERE user_id='$user_id'";
+
+        if (!$result = mysql_query($sql)) {
+
+            showerror();
+
+        } else {
+
+            $arrMessage[] = $user["username"] ." -> unbanned!";
+
+        };
+
+
+    } elseif ($user["perm"] != "banned") {
+
+        $arrMessage[] = "<span class=\"error\">". $user["username"] ." is not currently banned!</span>";
+
+    } else {
+
+        $arrMessage[] = "<span class=\"error\">the user_id provided is invalid</span>";
+
+    };
+
+    if (!empty($arrMessage)) {
+    
+        message("unbanuser", "information", $arrMessage);
+    
+    }; 
+};
 ?>
