b3400f3fa87e1c1ef146bdb4ee050b2761bd7908
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2008-04-06 05:53:31 +0000

    start using relative*and* absolute times! yeah!

--- profile.php
+++ profile.php
@@ -11,23 +11,34 @@ require_once "include_header.php";
 
 $user_id = clean($_GET["user_id"]);
 
-$sql = "SELECT user_id, username, avatar_type, title, profile, register_date, visit_date ".
-       "FROM ttf_user WHERE user_id='$user_id'";
+$sql = "SELECT user_id,         ".
+       "       username,        ".
+       "       avatar_type,     ".
+       "       title,           ".
+       "       profile,         ".
+       "       register_date,   ".
+       "       visit_date       ".
+       "FROM ttf_user           ".
+       "WHERE user_id='$user_id'";
 if (!$result = mysql_query($sql)) showerror();
 $user = mysql_fetch_array($result);
-mysql_free_result($result);
+
+$visit_date = formatdate($user["visit_date"]);
+$register_date = formatdate($user["register_date"]);
 
 // if a user was found with the given id
 if (isset($user["user_id"])) {
 
-    $sql = "SELECT COUNT(*) ".
-           "FROM ttf_post WHERE author_id='$user_id' && archive IS NULL";
+    $sql = "SELECT COUNT(*)             ".
+           "FROM ttf_post               ".
+           "WHERE author_id='$user_id'  ".
+           "   && archive IS NULL       ";
     if (!$result = mysql_query($sql)) showerror();
     list($numposts) = mysql_fetch_array($result);
-    mysql_free_result($result);
 
-    $sql = "SELECT COUNT(*) ".
-           "FROM ttf_thread WHERE author_id='$user_id'";
+    $sql = "SELECT COUNT(*)             ".
+           "FROM ttf_thread             ".
+           "WHERE author_id='$user_id'  ";
     if (!$result = mysql_query($sql)) showerror();
     list($numthreads) = mysql_fetch_array($result);
 
@@ -35,13 +46,19 @@ if (isset($user["user_id"])) {
             <div class="userbar">
                 <div class="userbar_left">
 <?php
+
     if (isset($user["avatar_type"])) {
+
 ?>
                     <img src="avatars/<?php echo $user["user_id"].".".$user["avatar_type"]; ?>" alt="av" width="30" height="30" />
 <?php
+
     } else {
+
         echo "                    &nbsp;\n";
+
     };
+
 ?>
                 </div>
                 <span class="username"><?php echo output($user["username"]); ?></span><br />
@@ -67,20 +84,25 @@ if (isset($user["user_id"])) {
                     </tr>
                     <tr>
                         <td>last visited</td>
-                        <td><?php echo formatdate($user["visit_date"]); ?></td>
+                        <td><span title="<?php echo $visit_date[1]; ?>"><?php echo $visit_date[0]; ?></span></td>
                     </tr>
                     <tr>
                         <td>account registered</td>
-                        <td><?php echo formatdate($user["register_date"]); ?></td>
+                        <td><span title="<?php echo $register_date[1]; ?>"><?php echo $register_date[0]; ?></span></td>
                     </tr>
                 </tbody>
             </table>
 <?php
-    $sql = "SELECT ttf_post.post_id, ttf_post.thread_id, ttf_post.date, ".
-           "       ttf_thread.title FROM ttf_post ".
-           "LEFT JOIN ttf_thread ON ttf_post.thread_id=ttf_thread.thread_id ".
-           "WHERE ttf_post.author_id='$user_id' && ttf_post.archive IS NULL ".
-           "ORDER BY date DESC LIMIT 5";
+    $sql = "SELECT ttf_post.post_id,                        ".
+           "       ttf_post.thread_id,                      ".
+           "       ttf_post.date,                           ".
+           "       ttf_thread.title                         ".
+           "FROM ttf_post                                   ".
+           "LEFT JOIN ttf_thread                            ".
+           "  ON ttf_post.thread_id=ttf_thread.thread_id    ".
+           "WHERE ttf_post.author_id='$user_id'             ".
+           "   && ttf_post.archive IS NULL                  ".
+           "ORDER BY date DESC LIMIT 5                      ";
     if (!$result = mysql_query($sql)) showerror();
     if (mysql_num_rows($result) != 0) {
 ?>
@@ -93,10 +115,13 @@ if (isset($user["user_id"])) {
                 <tbody>
 <?php
         while ($post = mysql_fetch_array($result)) {
+
+            $date = formatdate($post["date"]);
+
 ?>
                     <tr>
                         <td><a href="thread.php?thread_id=<?php echo $post["thread_id"]."#".$post["post_id"]."\">".$post["title"]; ?></a></td>
-                        <td><?php echo formatdate($post["date"]); ?></td>
+                        <td><span title="<?php echo $date[1]; ?>"><?php echo $date[0]; ?></span></td>
                     </tr>                                
 <?php
 
@@ -109,11 +134,9 @@ if (isset($user["user_id"])) {
 
     };
 
-    mysql_free_result($result);
-
 } else {
 
-    message("user profile","fatal error","you must specify a valid user.");
+    message($ttf_label, $error_die_text, "you must specify a valid user.");
 
 };
 
