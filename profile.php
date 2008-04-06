4aec0231f6b2296b162f530668f8c7f69454c21b
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2008-04-06 17:55:10 +0000

    more rev stats (profile, title) for users' profiles

--- profile.php
+++ profile.php
@@ -7,7 +7,7 @@
 require_once "include_common.php"; 
 $ttf_label = "user profile";
 $ttf_title = $ttf_label;
-require_once "include_header.php";     
+require_once "include_header.php";
 
 $user_id = clean($_GET["user_id"]);
 
@@ -23,41 +23,60 @@ $sql = "SELECT user_id,         ".
 if (!$result = mysql_query($sql)) showerror();
 $user = mysql_fetch_array($result);
 
+// if a user was not found with the given id
+if (empty($user["user_id"])) {
+
+    message($ttf_label, $error_die_text, "you must specify a valid user.");
+    die();
+
+};
+
 $visit_date = formatdate($user["visit_date"]);
 $register_date = formatdate($user["register_date"]);
 
-// if a user was found with the given id
-if (isset($user["user_id"])) {
+$sql = "SELECT COUNT(*)-1       ".
+       "FROM ttf_revision       ".
+       "WHERE ref_id='$user_id' ".
+       "   && type='profile'    ";
+if (!$result = mysql_query($sql)) showerror();
+list($profilerev) = mysql_fetch_array($result);
 
-    $sql = "SELECT COUNT(*)             ".
-           "FROM ttf_post               ".
-           "WHERE author_id='$user_id'  ".
-           "   && archive IS NULL       ";
-    if (!$result = mysql_query($sql)) showerror();
-    list($numposts) = mysql_fetch_array($result);
+$sql = "SELECT COUNT(*)-1       ".
+       "FROM ttf_revision       ".
+       "WHERE ref_id='$user_id' ".
+       "   && type='title'      ";
+if (!$result = mysql_query($sql)) showerror();
+list($titlerev) = mysql_fetch_array($result);
 
-    $sql = "SELECT COUNT(*)             ".
-           "FROM ttf_thread             ".
-           "WHERE author_id='$user_id'  ";
-    if (!$result = mysql_query($sql)) showerror();
-    list($numthreads) = mysql_fetch_array($result);
+$sql = "SELECT COUNT(*)             ".
+       "FROM ttf_post               ".
+       "WHERE author_id='$user_id'  ".
+       "   && archive IS NULL       ";
+if (!$result = mysql_query($sql)) showerror();
+list($numposts) = mysql_fetch_array($result);
+
+$sql = "SELECT COUNT(*)             ".
+       "FROM ttf_thread             ".
+       "WHERE author_id='$user_id'  ";
+if (!$result = mysql_query($sql)) showerror();
+list($numthreads) = mysql_fetch_array($result);
 
 ?>
             <div class="userbar">
                 <div class="userbar_left">
 <?php
 
-    if (isset($user["avatar_type"])) {
+if (isset($user["avatar_type"])) {
 
 ?>
                     <img src="avatars/<?php echo $user["user_id"].".".$user["avatar_type"]; ?>" alt="av" width="30" height="30" />
 <?php
 
-    } else {
+} else {
 
-        echo "                    &nbsp;\n";
+    echo "                    &nbsp;\n";
 
-    };
+};
 
 ?>
                 </div>
@@ -74,6 +93,18 @@ if (isset($user["user_id"])) {
                     </tr>
                 </thead>
                 <tbody>
+                    <tr>
+                        <td>
+                            <a href="revision.php?type=title&amp;ref_id=<?php echo $user["user_id"]; ?>">title revisions</a>
+                        </td>
+                        <td><?php echo $titlerev; ?></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="revision.php?type=profile&amp;ref_id=<?php echo $user["user_id"]; ?>">profile revisions</a>
+                        </td>
+                        <td><?php echo $profilerev; ?></td>
+                    </tr>
                     <tr>
                         <td>number of posts</td>
                         <td><?php echo $numposts; ?></td>
@@ -93,18 +124,18 @@ if (isset($user["user_id"])) {
                 </tbody>
             </table>
 <?php
-    $sql = "SELECT ttf_post.post_id,                        ".
-           "       ttf_post.thread_id,                      ".
-           "       ttf_post.date,                           ".
-           "       ttf_thread.title                         ".
-           "FROM ttf_post                                   ".
-           "LEFT JOIN ttf_thread                            ".
-           "  ON ttf_post.thread_id=ttf_thread.thread_id    ".
-           "WHERE ttf_post.author_id='$user_id'             ".
-           "   && ttf_post.archive IS NULL                  ".
-           "ORDER BY date DESC LIMIT 5                      ";
-    if (!$result = mysql_query($sql)) showerror();
-    if (mysql_num_rows($result) != 0) {
+$sql = "SELECT ttf_post.post_id,                        ".
+       "       ttf_post.thread_id,                      ".
+       "       ttf_post.date,                           ".
+       "       ttf_thread.title                         ".
+       "FROM ttf_post                                   ".
+       "LEFT JOIN ttf_thread                            ".
+       "  ON ttf_post.thread_id=ttf_thread.thread_id    ".
+       "WHERE ttf_post.author_id='$user_id'             ".
+       "   && ttf_post.archive IS NULL                  ".
+       "ORDER BY date DESC LIMIT 5                      ";
+if (!$result = mysql_query($sql)) showerror();
+if (mysql_num_rows($result) != 0) {
 ?>
             <table cellspacing="1" class="content">
                 <thead>
@@ -114,9 +145,9 @@ if (isset($user["user_id"])) {
                 </thead>
                 <tbody>
 <?php
-        while ($post = mysql_fetch_array($result)) {
+    while ($post = mysql_fetch_array($result)) {
 
-            $date = formatdate($post["date"]);
+        $date = formatdate($post["date"]);
 
 ?>
                     <tr>
@@ -125,18 +156,12 @@ if (isset($user["user_id"])) {
                     </tr>                                
 <?php
 
-        };
+    };
 
 ?>
                 </tbody>
             </table>
 <?php
-
-    };
-
-} else {
-
-    message($ttf_label, $error_die_text, "you must specify a valid user.");
 
 };
 
