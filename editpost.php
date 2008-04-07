0f789fb5058cb9a683a7423b3dd3675b96693395
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2008-04-07 19:14:21 +0000

    locking for editing posts

--- editpost.php
+++ editpost.php
@@ -14,6 +14,7 @@ require_once "include_common.php";
 // use $body in its raw-input form
 $post_id = $_REQUEST["post_id"];
 $body = $_POST["body"];
+$rev_num = $_POST["rev_num"];
 
 // if the agent is not logged in as a valid user
 if (!isset($ttf["uid"])) {
@@ -24,7 +25,7 @@ if (!isset($ttf["uid"])) {
 
 };
 
-// if a post_id is not specified
+// if a post is not specified
 if (empty($post_id)) {
 
     message($ttf_label, $error_die_text,
@@ -34,15 +35,15 @@ if (empty($post_id)) {
 };
 
 // let's check some permissions (either admin or author)
-if ($ttf["perm"] != "admin") {
-    
+if ($ttf["perm"] != 'admin') {
+
     $sql = "SELECT author_id FROM ttf_post ".
            "WHERE post_id='".clean($post_id)."'";
     if (!$result = mysql_query($sql)) showerror();
     list($author_id) = mysql_fetch_array($result);
 
     if ($ttf["uid"] != $author_id) {
-       
+
         message($ttf_label, $error_die_text,
                 "you do not have permission to edit this post.");
         die();
@@ -52,22 +53,40 @@ if ($ttf["perm"] != "admin") {
 };
 
 
+// get the $head of the post
+// and get the number of revisions
+$sql = "SELECT SQL_CALC_FOUND_ROWS          ".
+       "       body                         ".
+       "FROM ttf_revision                   ".
+       "WHERE ref_id='".clean($post_id)."'  ".
+       "   && type='post'                   ".
+       "ORDER BY date DESC LIMIT 1          ";
+
+if (!$result = mysql_query($sql)) showerror();
+list($head) = mysql_fetch_array($result);
+
+$sql = "SELECT FOUND_ROWS()";
+if (!$result = mysql_query($sql)) showerror();
+list($num_revs) = mysql_fetch_array($result);
+
 if (!empty($body)) {
-    
-    // let's get our current HEAD revision
 
-    $sql = "SELECT body FROM ttf_revision ".
-           "WHERE ref_id='".clean($post_id)."' && type='post' ".
-           "ORDER BY date DESC LIMIT 1";
-    if (!$result = mysql_query($sql)) showerror();
-    list($head) = mysql_fetch_array($result);
-
-    if (empty($head)) {
+    if (empty($head) || empty($num_revs)) {
 
         message($ttf_label, $error_die_text,
                 "serious error encountered. please contact an admin.");
         die();
-    
+
+    };
+
+    if ($rev_num != $num_revs) {
+
+        message($ttf_label, $error_die_text,
+            "while you were editing the post, someone else committed a revision. ".
+            "please go back, save your changes, reload the edit page, then merge ".
+            "in your changes with the current version by hand.");
+        die();
+
     };
 
     if ($body == $head) {
@@ -75,9 +94,9 @@ if (!empty($body)) {
         message($ttf_label, $error_die_text,
                 "you didn't make any changes.");
         die();
-    
+
     };
-    
+
     $sql = "INSERT INTO ttf_revision SET ".
            "ref_id='".clean($post_id)."', ".
            "type='post', ".
@@ -91,38 +110,22 @@ if (!empty($body)) {
     $sql = "UPDATE ttf_post SET rev=rev+1, ".
            "body='".clean(outputbody($body))."' WHERE post_id='".clean($post_id)."'";
     if (!$result = mysql_query($sql)) showerror();
-    
+
     // wow, all of that worked! let's grab the thread_id
     // and redirect the user to their edited post
     $sql = "SELECT thread_id FROM ttf_post ".
            "WHERE post_id='".clean($post_id)."'";
     if (!$result = mysql_query($sql)) showerror();
     list($thread_id) = mysql_fetch_array($result);
-    
+
     header("Location: thread.php?thread_id=".$thread_id."#".$post_id);
 
 } else if (!isset($_POST["body"])) {
-    
+
     $title = "editing post_id $post_id";
     $label = $title;
     require_once "include_header.php";
-    
-    $sql = "SELECT SQL_CALC_FOUND_ROWS          ".
-           "       body                         ".
-           "FROM ttf_revision                   ".
-           "WHERE ref_id='".clean($post_id)."'  ".
-           "   && type='post'                   ".
-           "ORDER BY date DESC LIMIT 1          ";
-    if (!$result = mysql_query($sql)) showerror();
-    list($head) = mysql_fetch_array($result);
 
-    $sql = "SELECT FOUND_ROWS()";
-    if (!$result = mysql_query($sql)) showerror();
-    list($num_revs) = mysql_fetch_array($result);
-
-    // the following html kinda needs some work,
-    // but i'm lazy about that right now.. i just
-    // want to get this feature working perfectly! --jlr
 ?>
             <form action="editpost.php" method="post">
                 <div class="contenttitle">you're creating revision <?php echo $num_revs; ?> of post <?php echo $post_id; ?></div>
@@ -133,6 +136,7 @@ if (!empty($body)) {
                     <input class="editpost" type="submit" value="submit revision" />
                 </div>
                 <input type="hidden" name="post_id" value="<?php echo $post_id; ?>" />
+                <input type="hidden" name="rev_num" value="<?php echo $num_revs; ?>" />
             </form>
 <?php
 
