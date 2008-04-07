bc1ac4f4a2761f08f532ff6039db78f930dfd2cc
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2008-04-07 19:50:17 +0000

    admin scripts in progress..

--- admin_userlist.php
+++ admin_userlist.php
@@ -6,28 +6,12 @@
 
 require_once "include_common.php";
 
-// if an admin isn't logged in, then die()!
 admin();
 
-$label = "administration &raquo; user list";
+$ttf_label = "administration &raquo; user list";
+$ttf_title = $ttf_label;
 
 require_once "include_header.php";
-
-
-if (isset($_GET["banuser"])) {
-        
-    $user_id = clean($_GET["banuser"]);
-
-    banUser($user_id);
-}
-
-else if (isset($_GET["unbanuser"])) {
-        
-    $user_id = clean($_GET["unbanuser"]);
-
-    unbanUser($user_id);
-
-} else { 
 
 ?>
             <table cellspacing="1" class="content">
@@ -37,47 +21,55 @@ else if (isset($_GET["unbanuser"])) {
                         <th>username</th>
                         <th>email</th>
                         <th>last visit</th>
-                        <th>ban user</th>
+                        <th>action</th>
                     </tr>
                 </thead>
                 <tbody>
 <?php
 
-$sql = "SELECT user_id, username, email, visit_date, perm ".
-       "FROM ttf_user ORDER BY user_id";
+$sql = "SELECT user_id,     ".
+       "       username,    ".
+       "       email,       ".
+       "       visit_date,  ".
+       "       perm         ".
+       "FROM ttf_user       ".
+       "ORDER BY user_id    ";
 if (!$result = mysql_query($sql)) showerror();
 
 while ($user = mysql_fetch_array($result)) {
 
-    unset($hl);
-
+    // highlight visitors within the last two weeks
+    unset($visithl);
     $timeout = 60*60*24*7*2; // two weeks of seconds (s*m*h*d*w)
-
     if ($user["visit_date"] > (time() - $timeout)) {
-
-        $hl = " class=\"highlight\"";
-
+        $visithl = " class=\"highlight\"";
     };
 
+    // highlight admins and lowlight banned users
+    unset($userhl);
+    if ($user["perm"] == 'admin') {
+        $userhl = " class=\"highlight\"";
+    } else if ($user["perm"] == 'banned') {
+        $userhl = " class=\"lowlight\"";
+    };
+    
+    $date = formatdate($user["visit_date"]);
+
+    // make actions to show or use
+    unset($actions);
+    if ($user["perm"] == 'user') {
+        $actions .= "<a href=\"admin_banuser.php?action=ban&amp;user_id=".$user["user_id"]."\">ban</a>";
+    } else if ($user["perm"] == 'banned') {
+        $actions .= "<a href=\"admin_banuser.php?action=unban&amp;user_id=".$user["user_id"]."\">unban</a>";
+    };
 
 ?>
                     <tr>
                         <td><?php echo $user["user_id"]; ?></td>
-                        <td><a href="admin_userinfo.php?user_id=<?php echo $user["user_id"]; ?>"><?php echo output($user["username"]); ?></a></td>
+                        <td<?php echo $userhl; ?>><a href="admin_userinfo.php?user_id=<?php echo $user["user_id"]; ?>"><?php echo output($user["username"]); ?></a></td>
                         <td><a href="mailto:<?php echo $user["email"]; ?>"><?php echo $user["email"]; ?></a></td>
-                        <td<?php echo $hl; ?>><?php echo formatdate($user["visit_date"]); ?></td>
-                        <td><a href="admin_user.php?<?php 
-                            if ($user["perm"] != "banned") {
-
-                                echo "banuser=".$user["user_id"]."\">ban</a>";
-
-                            } else {
-                                
-                                echo "unbanuser=".$user["user_id"]."\">unban</a>";
-
-                            }
-
-                            ?>
+                        <td<?php echo $visithl; ?>><span title="<?php echo $date[1]; ?>"><?php echo $date[0]; ?></span></td>
+                        <td>
                         </td>
                             
                     </tr>
@@ -85,14 +77,10 @@ while ($user = mysql_fetch_array($result)) {
 
 };
 
-mysql_free_result($result);
-
 ?>
                 </tbody>
             </table>
 <?php
-
-};
 
 require_once "include_footer.php";
 
