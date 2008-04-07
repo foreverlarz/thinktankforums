667e526792913a208e9decfba2d459e412cbf19d
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2008-04-07 06:11:00 +0000

    fix up archivepost.php

--- archivepost.php
+++ archivepost.php
@@ -26,7 +26,9 @@ if (empty($post_id)) {
 };
 
 // archive the post if the user is either an admin or the post's author
-$sql = "UPDATE ttf_post SET archive=UNIX_TIMESTAMP() WHERE post_id=$post_id";
+$sql = "UPDATE ttf_post                 ".
+       "SET archive=UNIX_TIMESTAMP()    ".
+       "WHERE post_id='$post_id'        ";
 if ($ttf["perm"] != 'admin') $sql .= " AND author_id='{$ttf["uid"]}'";
 if (!$result = mysql_query($sql)) showerror();
 
@@ -38,30 +40,44 @@ if (mysql_affected_rows() != 1) {
 };
 
 // find out the thread_id for the given post
-$sql = "SELECT thread_id FROM ttf_post WHERE post_id=$post_id";
+$sql = "SELECT thread_id        ".
+       "FROM ttf_post           ".
+       "WHERE post_id='$post_id'";
 if (!$result = mysql_query($sql)) showerror();
 list($thread_id) = mysql_fetch_array($result);
+
+// find out the forum_id for the given thread
+$sql = "SELECT forum_id             ".
+       "FROM ttf_thread             ".
+       "WHERE thread_id='$thread_id'";
+if (!$result = mysql_query($sql)) showerror();
+list($forum_id) = mysql_fetch_array($result);
     
 // update the thread table, subtracting a post from the count
 // and setting the date to the date of the most recent post in the thread
-// WORD UP ==> IF THE THREAD HAS NO OTHER POSTS, date->0, listing it last. --jlr *********************
-$sql = "UPDATE ttf_thread SET posts=posts-1, ".
-    "date=(SELECT date FROM ttf_post ".
-    "      WHERE thread_id=$thread_id AND hide='f' ".
-    "      ORDER BY date DESC LIMIT 1) ".
-    "WHERE thread_id=$thread_id LIMIT 1";
+// WORD UP ==> IF THE THREAD HAS NO OTHER POSTS, date->0, listing it last.
+$sql = "UPDATE ttf_thread                       ".
+       "SET posts=posts-1,                      ".
+       "    date=(SELECT date                   ".
+       "          FROM ttf_post                 ".
+       "          WHERE thread_id='$thread_id'  ".
+       "             && archive IS NULL         ".
+       "          ORDER BY date DESC LIMIT 1)   ".
+       "WHERE thread_id='$thread_id' LIMIT 1    ";
 if (!$result = mysql_query($sql)) showerror();
 
 // update the forum table, subtracting a post from the count
 // and setting the date to the date of the most recent post in the forum
-// WORD UP ==> IF THE FORUM HAS NO OTHER POSTS, date->0, listing it last. --jlr **********************
-$sql = "UPDATE ttf_forum SET posts=posts-1, ".
-    "date=(SELECT ttf_post.date FROM ttf_post, ttf_thread ".
-    "      WHERE ttf_thread.thread_id=ttf_post.thread_id ".
-    "      && ttf_thread.forum_id=1 && ttf_post.hide='f' ".
-    "      ORDER BY ttf_post.date DESC LIMIT 1) ".
-    "WHERE forum_id=(SELECT forum_id FROM ttf_thread ".
-    "                WHERE thread_id=$thread_id)";
+// WORD UP ==> IF THE FORUM HAS NO OTHER POSTS, date->0, listing it last.
+$sql = "UPDATE ttf_forum                                        ".
+       "SET posts=posts-1,                                      ".
+       "    date=(SELECT ttf_post.date                          ".
+       "          FROM ttf_post, ttf_thread                     ".
+       "          WHERE ttf_thread.thread_id=ttf_post.thread_id ".
+       "             && ttf_thread.forum_id='$forum_id'         ".
+       "             && ttf_post.archive IS NULL                ".
+       "          ORDER BY ttf_post.date DESC LIMIT 1)          ".
+       "WHERE forum_id='$forum_id'                              ";
 if (!$result = mysql_query($sql)) showerror();
 
 /* NOTE: this script does not revert back the author's
