8c401e0a8ae4387451992d2c05c7c7f30ceb798a
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2008-10-11 01:36:52 +0000

    clean up: activate.php.

--- activate.php
+++ activate.php
@@ -4,18 +4,23 @@
  * activate.php
  */
 
-require_once "include_common.php";   
-$ttf_label = "Activate Your Password";
-$ttf_title = $ttf_label;
-require_once "include_header.php";
+$ttf_title = $ttf_label = "activate your password";
 
-kill_users();
+require_once "include_common.php";   
+
+// people with accounts don't need to activate
+kill_users(); 
+
+require_once "include_header.php";
 
 $passkey = clean($_POST["passkey"]);
 
+
+
+// if we were given a passkey...
 if (!empty($passkey)) {
 
-    // get the information from the passkey
+    // select the information from the passkey
     $sql = "SELECT user_id,         ".
            "       password         ".
            "FROM ttf_recover        ".
@@ -23,6 +28,9 @@ if (!empty($passkey)) {
 
     if (!$result = mysql_query($sql)) showerror();
 
+
+
+    // if there isn't exactly one match, we have serious problems. ABORT!
     if (mysql_num_rows($result) !== 1) {
 
         message($ttf_label, $ttf_msg["fatal_error"], $ttf_msg["passkeydne"]);
@@ -30,7 +38,12 @@ if (!empty($passkey)) {
 
     };
 
+
+
+    // otherwise, grab the infos
     list($user_id, $password) = mysql_fetch_array($result);
+
+
 
     // use the information to change the active password
     $sql = "UPDATE ttf_user             ".
@@ -38,22 +51,27 @@ if (!empty($passkey)) {
            "WHERE user_id='$user_id'    ";
     if (!$result = mysql_query($sql)) showerror();
 
+
+
     // get rid of the recovery record
     $sql = "DELETE FROM ttf_recover WHERE passkey='$passkey'";
     if (!$result = mysql_query($sql)) showerror();
 
+
+
+    // tell the user it worked, kill the agent
     message($ttf_label, $ttf_msg["successtitl"], $ttf_msg["pwdchanged"]);
     die();
 
 };
 
 ?>
-            <div class="contenttitle">Activate Your Password</div>
+            <div class="contenttitle">activate your password</div>
             <div class="contentbox">
                 <form action="activate.php" method="post">
-                    Enter your passkey to activate your new password.<br /><br />
+                    enter your passkey to activate your new password.<br /><br />
                     <input type="text" name="passkey" size="36" /><br /><br />
-                    <input type="submit" value="Activate" />
+                    <input type="submit" value="activate" />
                 </form>
             </div>
 <?php
