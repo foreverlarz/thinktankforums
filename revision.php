b8cb1e644b988b8e72d6b2b43cb3318f8e2e4095
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2008-10-11 00:43:32 +0000

    clean up: revision.php.

--- revision.php
+++ revision.php
@@ -4,13 +4,15 @@
  * revision.php
  */
 
-$ttf_label = "Revision Browser";
+$ttf_title = $ttf_label = "revision browser";
 
 require_once "include_common.php";
 
 // pull through the variables
 $ref_id = clean($_GET["ref_id"]);
 $type   = clean($_GET["type"]);
+
+
 
 // if either of the variables is empty
 if (empty($ref_id) || empty($type)) {
@@ -20,22 +22,36 @@ if (empty($ref_id) || empty($type)) {
 
 };
 
+
+
+// make some nice titles/labels
 switch ($type) {
 case 'post':
-    $ttf_label .= " &raquo; Post for pid $ref_id";
+    $ttf_label .= " &raquo; post $ref_id";
     break;
 case 'profile':
-    $ttf_label .= " &raquo; Profile for uid $ref_id";
+    $ttf_label .= " &raquo; profile for user $ref_id";
     break;
 case 'title':
-    $ttf_label .= " &raquo; Title for uid $ref_id";
+    $ttf_label .= " &raquo; title for user $ref_id";
     break;
 };
+
 $ttf_title = $ttf_label;
+
+
+
+// let's output a page to the user
 require_once "include_header.php";
 
+
+
+// start counting revisions
 $revnum = 0;
 
+
+
+// grab the revisions
 $sql = "SELECT ttf_revision.*, ttf_user.username ".
        "FROM ttf_revision, ttf_user ".
        "WHERE ttf_revision.author_id = ttf_user.user_id ".
@@ -49,8 +65,10 @@ while ($rev = mysql_fetch_array($result)) {
 
     echo "            <div class=\"contenttitle_sm\">\n";
     echo "                rev $revnum, rev_id {$rev["rev_id"]} by\n";
-    echo "                <a class=\"link\" href=\"profile.php?user_id={$rev["author_id"]}\">";
-    echo output($rev["username"])."</a> ({$rev["ip"]})\n";
+    echo "                <a class=\"link\" href=\"profile.php?user_id={$rev["author_id"]}\">".output($rev["username"])."</a>";
+    if (!empty($rev["ip"])) {
+        echo "                ({$rev["ip"]})\n";
+    };
     echo "                <span title=\"{$date[1]}\">{$date[0]}</span>\n";
     echo "            </div>\n";
     echo "            <div class=\"contentbox_sm\">\n";
@@ -60,6 +78,8 @@ while ($rev = mysql_fetch_array($result)) {
     $revnum++;
 
 };
+
+
 
 require_once "include_footer.php";
 
