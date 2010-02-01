7157ce7c818f6241a4b76c9e3b7057783d15b881
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2010-02-01 11:03:08 +0000

    rss.php now gives contextual links.
    AS PER REQUEST FROM RYAN GANTT.

--- rss.php
+++ rss.php
@@ -19,19 +19,38 @@ header("Content-Type: application/xml; charset=utf-8");
 
     echo <<<EOF
 <?xml version="1.0" encoding="utf-8" ?>
-<rss version="2.0"> 
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel> 
         <title>{$ttf_cfg['forum_shortname']} -- latest revisions</title>
         <link>http://{$ttf_cfg['address']}/latest.php</link>
         <description>latest revisions</description>
+        <atom:link href="$ttf_protocol://{$ttf_cfg["address"]}/rss.php" rel="self" type="application/rss+xml" />
 
 EOF;
 
 while ($rev = mysql_fetch_array($result)) {
 
+    if ($rev['type'] === 'post') {
+        $sql = "SELECT thread_id FROM ttf_post WHERE post_id='{$rev['ref_id']}'";
+        if (!$result_a = mysql_query($sql)) showerror();
+        list($thread_id) = mysql_fetch_row($result_a);
+        $bonus = "<a href=\"http://{$ttf_cfg['address']}/thread.php?thread_id=$thread_id#post-{$rev['ref_id']}\">view this post in the context of the thread</a>";
+    } else if ($rev['type'] === 'thread') {
+        $sql = "SELECT thread_id FROM ttf_post WHERE post_id='{$rev['ref_id']}'";
+        if (!$result_a = mysql_query($sql)) showerror();
+        list($thread_id) = mysql_fetch_row($result_a);
+        $bonus = "<a href=\"http://{$ttf_cfg['address']}/thread.php?thread_id=$thread_id\">view this thread</a>";
+    } else if ($rev['type'] === 'profile') {
+        $bonus = "<a href=\"http://{$ttf_cfg['address']}/profile.php?user_id={$rev['ref_id']}\">view this user profile</a>";
+    } else if ($rev['type'] === 'title') {
+        $bonus = "<a href=\"http://{$ttf_cfg['address']}/profile.php?user_id={$rev['ref_id']}\">view this user title in the context of the profile</a>";
+    };
+
     $title = outputxml("{$rev['type']} {$rev['ref_id']}, rid {$rev['rev_id']} by {$rev['username']}");
     $link = outputxml("http://{$ttf_cfg['address']}/revision.php?type={$rev['type']}&ref_id={$rev['ref_id']}");
-    $description = outputxml(nl2br($rev['body']));
+    $description = outputxml(nl2br($rev['body']."\n\n<hr \>".$bonus));
+
+
 
     echo <<<EOF
         <item>
