197dec1c1ad3ce81b12ca492522f5268b8077d84
lucasreddinger <lucasreddinger@510118ab-9126-0410-8522-355556fb886b>
2012-11-11 09:17:48 +0000

    simplify logout procedure.

--- logout.php
+++ logout.php
@@ -8,7 +8,7 @@ require_once "include_common.php";
 
 if (isset($_COOKIE["{$ttf_cfg["cookie_name"]}-pair"]) || isset($_COOKIE["{$ttf_cfg["cookie_name"]}-user"])) {
 
-    $expire = time() - 3600;
+    $expire = 1;
 
     setcookie($ttf_cfg["cookie_name"].'-pair', '', $expire, $ttf_cfg["cookie_path"], $ttf_cfg["cookie_domain"], $ttf_cfg["cookie_secure"]);
     setcookie($ttf_cfg["cookie_name"].'-user', '', $expire, $ttf_cfg["cookie_path"], $ttf_cfg["cookie_domain"], FALSE);
